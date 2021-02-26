4ae5a46f6b7f5dd00d3f570149538e589707e3a5
hrozan <higorb.rozan@gmail.com>
2021-02-26 15:26:03 -0300

    :new: Add alias.sh

---
+++ alias.sh
@@ -0,0 +1,9 @@
+# Config
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias nvimconfig="nvim ~/.config/nvim/init.vim"
+
+# Change ls to exa
+alias ls='exa'
+alias ll='exa -lh --git'
+alias la='exa -lh --git -a '
