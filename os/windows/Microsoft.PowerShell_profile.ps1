828945b1264b0737d0e5d4036df800dd6bf97fa4
Higor Rozan <higorb.rozan@gmail.com>
2022-06-26 10:36:58 -0300

    feat: add z to powershell profile

--- os/windows/Microsoft.PowerShell_profile.ps1
+++ os/windows/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,16 @@
+# Author: Higor Rozan
 
-# Start Starship
 Invoke-Expression (&starship init powershell)
 
-# Complete
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-# Terminal Icons
-Import-Module -Name Terminal-Icons
-
-# Fzf
+Import-Module Terminal-Icons
 Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Import-Module z
 
-# PSReadLine
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 
-# Alias
 Set-Alias g git
 Set-Alias s scoop
 Set-Alias n npm
