015fd7e444156c2e0b456bf80ed33bdb8cae7587
Higor Rozan <higorb.rozan@gmail.com>
2022-07-31 19:35:09 -0300

    feat: add alias

--- os/linux/zshrc.sh
+++ os/linux/zshrc.sh
@@ -24,10 +24,12 @@ source /usr/share/nvm/init-nvm.sh
 
 alias g=git
 alias n=npm
+alias y=yarn
 alias c=clear
 alias vi=nvim
 alias http=curlie
 alias vim=neovide
+alias cat='bat -p --theme=OneHalfDark'
 alias l='exa --long --header --git --icons'
 alias la='exa --long --header --git --icons --all'
 alias dc='docker-compose'
