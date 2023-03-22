2195604d92418b3fcf6b94c2448135c97b5d9d0e
Higor Bassi <higorb.rozan@lambda3.com.br>
2023-03-22 09:54:46 -0300

    update kitty and zshrc

--- zshrc.sh
+++ zshrc.sh
@@ -4,17 +4,18 @@ PATH=$HOME/.local/bin:$HOME/.cargo/bin:$PATH
 EDITOR=nvim
 ZSH=$HOME/.oh-my-zsh/
 plugins=(
-  git
-  gh
-  fzf-zsh-plugin
-  zsh-syntax-highlighting
-  node
-  npm
-  nvm
-  docker
-  docker-compose
-  github
-  dotnet
+	git
+	gh
+	fzf-zsh-plugin
+	zsh-syntax-highlighting
+	node
+	npm
+	nvm
+	docker
+	docker-compose
+	github
+	dotnet
+	rust
 )
 
 eval "$(starship init zsh)"
@@ -27,9 +28,10 @@ alias n=npm
 alias d=dotnet
 alias c=clear
 alias vi=nvim
+alias vim=neovide
 alias gs='g s'
 alias http=curlie
 alias cat='bat -p --theme=OneHalfDark'
 alias l='exa --long --header --git --icons'
 alias la='exa --long --header --git --icons --all'
-
+alias lg=lazygit
